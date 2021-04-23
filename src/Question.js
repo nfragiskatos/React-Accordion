c22cb983213a753ba1b6cf74c4e2a87e23e5b490
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-23 12:07:47 -0700

    Fixed styling issue

--- src/Question.js
+++ src/Question.js
@@ -3,17 +3,15 @@ import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({ question }) => {
 	const [ isCollapsed, setIsCollapsed ] = useState(true);
 	return (
-		<main>
-			<section className="question">
-				<header>
-					<h4>{question.title}</h4>
-					<button className="btn" onClick={() => setIsCollapsed(!isCollapsed)}>
-						{isCollapsed ? <AiOutlinePlus /> : <AiOutlineMinus />}
-					</button>
-				</header>
-				<p>{isCollapsed ? '' : question.info}</p>
-			</section>
-		</main>
+		<section className="question">
+			<header>
+				<h4>{question.title}</h4>
+				<button className="btn" onClick={() => setIsCollapsed(!isCollapsed)}>
+					{isCollapsed ? <AiOutlinePlus /> : <AiOutlineMinus />}
+				</button>
+			</header>
+			<p>{isCollapsed ? '' : question.info}</p>
+		</section>
 	);
 };
 
