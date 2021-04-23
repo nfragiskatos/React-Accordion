1664a9d18e2a24d34d01e1c616827e1d7c08b19f
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-23 12:01:48 -0700

    Initial layout of Question.js
    - No state or event handling. Returns elements displaying all question information.

--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({ question }) => {
-	return <h2>question component</h2>;
+	return (
+		<main>
+			<section className="question">
+				<header>
+					<h4>{question.title}</h4>
+					<button className="btn">
+						<AiOutlinePlus />
+					</button>
+				</header>
+				<p>{question.info}</p>
+			</section>
+		</main>
+	);
 };
 
 export default Question;
