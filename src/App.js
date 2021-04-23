944a8148bf6b05bf146970577b01482c1847fc31
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-23 11:35:21 -0700

    Setup initial App layout
    App.js
    - Setups a main with a section that contains a header, and a div which maps through the data and returns a Question component for each, and also passing in single question data as props.
    
    Question.js
    - Accepts props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
-function App() {
-  return <h2>accordion project setup</h2>;
+function App () {
+	return (
+		<main>
+			<section className="container">
+				<h3>Questions and answers about login</h3>
+				<div>
+					{data.map((q) => {
+						return <SingleQuestion question={q} />;
+					})}
+				</div>
+			</section>
+		</main>
+	);
 }
 
 export default App;
--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ question }) => {
+	return <h2>question component</h2>;
 };
 
 export default Question;
